8f355f0d9defe017a07bd08ba40fe3c21f5b3163
JustinMitchell <mitchelljm04@gmail.com>
2022-02-03 20:25:59 -0500

    commit tree 6b0216aa4b791040fd7f64a3431b25c17d3f0df1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findingMatch(drivers, query){
+    return drivers.filter(function(drivers){
+        return drivers.toLowerCase() === query.toLowerCase();
+    })
+};
+
+
+
+
